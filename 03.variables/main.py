7918c4a065d8555438302dab2a08f02094736081
rafsun7251 <golamrafsun7251@gmail.com>
2026-07-23 22:26:44 +0600

    Variables

---
+++ 03.variables/main.py
@@ -0,0 +1,19 @@
+Name = "Golam Rafsun"   #str
+Department = "CSE"     #str
+Age = 24     #int
+CGPA = 3.12       #float
+Credit_Completed = 85   #int
+Csharp_Completed = True    #bool
+
+print(Name)
+print(type(Name))
+print(Department)
+print(type(Department))
+print(Age)
+print(type(Age))
+print(CGPA)
+print(type(CGPA))
+print(Credit_Completed)
+print(type(Credit_Completed))
+print(Csharp_Completed)
+print(type(Csharp_Completed))
--- README.md
+++
@@ -1,2 +0,0 @@
-# Learning-Python
-Here I start to learn python. I upload my all code in here.
