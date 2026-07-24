4dbdc30ee6f68d9e26ae0e810abee4ee4b66cfda
rafsun7251 <golamrafsun7251@gmail.com>
2026-07-24 11:56:49 +0600

    Day 02

---
+++ 04. taking user input/main.py
@@ -0,0 +1,14 @@
+a = input("Enter Your Name: ")
+b = input("Enter ID: ")
+print(a)
+print(b)
+
+c = int(input("Enter your makrs: "))
+d= int(input("Enter your friend marks: "))
+
+print(c + d)
+
+
+e= input("Enter your First Name: ")
+f= input("Enter your Last Name: ")
+print(e+f)
