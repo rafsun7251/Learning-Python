762a5c2faf708641df0f2e92bdc8620073796402
rafsun7251 <golamrafsun7251@gmail.com>
2026-07-28 20:58:25 +0600

    Day 6

---
+++ 06. Comments, Escape/escape/escape/escape&print.py
@@ -0,0 +1,14 @@
+print("Hello, I am Rafsun \nI am a undergaduate student \nDOB: 03\\01\\2004")
+print("This is a tab \t character")
+#\n : Newline
+#\t : Tab
+#\\ : Backslash
+#\" : Double quote
+#\' : Single quote.
+#print
+print("Hello", "Rafsun", sep="/")
+print("Hello", "Rafsun", sep=",")
+print("Hello", "Rafsun", end="\n")
+print("Hello", "Rafsun", end="..")
+print("Hello", "Rafsun", end="//")
+
