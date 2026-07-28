58f636e74ed2121e0ad41ac759659901eab1934a
rafsun7251 <golamrafsun7251@gmail.com>
2026-07-28 22:40:21 +0600

    Day 6.1

---
+++ 07. Operators/operators.py
@@ -0,0 +1,61 @@
+a=10
+b=15
+c=5
+d=20
+#Arithmetic Operation 
+print("Arithmetic Operation")
+print("a + b = ", a+b)
+print("c - a =", c-d)
+print("d//a= ", d//a) #division without point value
+print("d^c= ", d**c) #power
+print("c%d=", c%d)
+print("a*b=", a*b)
+print("d/a= ", d/a) #division with point value
+
+#Comaprison  Operators
+print("Conditional Operators")  #== equal = holds value != not equal 
+print("a>b=", a>b)
+print("c<=d= ", c<=d)
+print("c value 5= ", c==5 )
+print("a value 20= ", a==20)
+print("d not equal 20=", d!=5)
+print("d not equal 10= ", d!=10)
+
+#Lofical operators
+#and, if any one false then return false
+#or , if any one true then return true
+#not, it's works in reverse mean if false then return true
+print("Logical operators") 
+print("AND")
+print("True and False =", True and False)
+print("True and True =", True and True) 
+print("False and True = ", False  and True)
+print("False and False=", False and False)
+print("OR")
+print("True or False =", True or False)
+print("True or False =", True or False)
+print("True or False =", True or False)
+print("False or False=", False or False)
+print("NOT")
+print("Not True= ", not True)
+print("Not False= ", not False)
+
+
+#Assignment operators
+print("Assignment operators")
+f=4
+g=2
+f+=g 
+print("f+=g =",f)
+f-=g
+print("f-=g =", f)
+f*=3
+print("f*=g =",f)
+f/=5
+print("f/=g =",f)
+f%=4
+print("f%=g =",f)
+f**=3
+print("f**=g =", f)
+f//=5
+print("f//g =", f)
