756e2482090e7a55af6539f4df13adabf06748bd
rafsun7251 <golamrafsun7251@gmail.com>
2026-07-30 01:01:52 +0600

    Day 7

---
+++ Practice Set/practice.py
@@ -0,0 +1,41 @@
+#question-01
+print("Q-01 \nHello, World! Welcome to Python")
+
+#Question-02
+print("Q-02 \nTwinkle, twinkle, little star, \nHow I wonder what you are! ")
+
+#Question-03
+name = "Golam Rafsun"
+age = 23
+height = 170.3
+is_student = True
+print("Q-03 \nStudent Name: " + name + "\nAge: " +str(age)+ "\nHeight: " + str(height)+ "\nStudent: "+str(is_student))
+
+#Question-04
+num = "45"
+print("Q-04")
+print(int(num)+10)
+
+#Question-05
+print("Q-04")
+food = input("Enter your favourite food: ")
+print("Wow! I also like this" +food)
+
+#Question-06
+print("Q-06")
+a= int(input("Enter your first number: "))
+b= int(input("Enter your second number: "))
+print("a+b= ", a+b)
+("a-b= ", a-b)
+("a*b= ", a*b)
+print("a/b= ", a/b)
+
+#Question-06
+print("Q-06")
+print("Harry said, \"Python is awesome\" \nThis is on a newline. \nThis is tab -> \t -< here")
+
+#Question-08
+print("Q-08")
+a= int(input("Write value of a : "))
+print("Square of a : ", a**2)
+print("Square of a : ", a**3)
