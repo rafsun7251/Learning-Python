320f403eaa57f7231881f6cbb044c4ebb88ca1ff
rafsun7251 <golamrafsun7251@gmail.com>
2026-07-31 00:32:15 +0600

    Day 7

---
+++ 08. Conditions/conditions.py
@@ -0,0 +1,8 @@
+age = int(input("Write your age: "))
+
+if(age<18):
+    print("Sorry dear, You can't drive now")
+elif(age == 18):
+    print("Please apply in for license at BRTA office")
+else: 
+    print("Congrat! You can drive")
