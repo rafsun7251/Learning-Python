bcc7c7b49e1556fe8381fbe4a7eaf08a9f37b577
rafsun7251 <golamrafsun7251@gmail.com>
2026-08-02 22:26:44 +0600

    Day 10.1

---
+++ 10. Strings/fstrings.py
@@ -0,0 +1,15 @@
+#strings formatting
+name = "Golam Rafsun"
+live_in = "Mirpur, Dhaka"
+mail = "golamrafsun01@gmail.com"
+study = "AIUB"
+
+print(f"Hello my name is {name}. I live in {live_in}. For contact me please mail me. My mail is {mail}. Currently, I study at {study}. ")
+#another formula 
+print("Hello, mu name is {}. I live in {}. For contact me please mail me. My mail is {}. Currently, I study at {}.".format(name, live_in,mail,study))
+
+#ord()/ char()
+# It based on ASCII code
+print(ord('A')) # output : 65
+print(chr(65))  # output : A
+
