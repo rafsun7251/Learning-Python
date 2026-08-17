198e6ced8dcaa327d80631072214335c8580651b
rafsun7251 <golamrafsun7251@gmail.com>
2026-08-17 23:10:39 +0600

    Funstion 1.2

---
+++ 11 Function/arguments.py
@@ -0,0 +1,16 @@
+#default arguments
+def add(a,b,c,plus=2): #plus=2 is default arguments
+    return a+b-c   #a,b,c = parameter
+d= add(3,4,1)       #3,4,1= arguments
+print(d)
+
+def greet(name="Rafsun"):
+    return f"Hello , {name}!"
+print(greet())
+
+#keywords arguments
+
+def student(name, age):
+    print(f"Name : {name}, Age: {age}")
+student(name= "Rafsun", age=20)
+          
